1577d3bf003e0224c1d604d206053cdbce6d5b7c
Tao <taole.chen@gmail.com>
2025-09-20 11:53:36 +0200

    reformat

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -2,7 +2,11 @@ import "./globals.css";
 import { Montserrat } from "next/font/google";
 import { Metadata } from "next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-
+import Footer from "../components/globals/Footer";
+import Header from "../components/globals/Header";
+import DotGrid from "../components/styledComponents/DotGrid";
+import DashedGutterOverlay from "./home/_components/DashedGutterOverlay";
+import { fetchLogos } from "@/lib/fetchers/graphicsFetchers";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -14,27 +18,43 @@ type Props = {
 };
 
 export const metadata: Metadata = {
-  title: 'Taole Chen - Art Portfolio',
-  description: 'The art portfolio of Taole Chen, a contemporary artist exploring the world and architecture through travel, plein air painting and mostly ink.',
-  keywords: ['Taole Chen', 'Art Portfolio', 'Contemporary Art', 'Ink Art', 'Digital Art', 'Architectural Drawing', 'Landscape Art', 'Travel Art', 'Mixed Media', 'Art Commissions', "Prints", "artist", "originals"],
+  title: "Taole Chen - Art Portfolio",
+  description:
+    "The art portfolio of Taole Chen, a contemporary artist exploring the world and architecture through travel, plein air painting and mostly ink.",
+  keywords: [
+    "Taole Chen",
+    "Art Portfolio",
+    "Contemporary Art",
+    "Ink Art",
+    "Digital Art",
+    "Architectural Drawing",
+    "Landscape Art",
+    "Travel Art",
+    "Mixed Media",
+    "Art Commissions",
+    "Prints",
+    "artist",
+    "originals",
+  ],
   alternates: {
     canonical: `${process.env.NEXT_PUBLIC_BASE_URL}`,
   },
-  authors: [{ name: 'Taole Chen', url: `${process.env.NEXT_PUBLIC_BASE_URL}` }],
-  creator: 'Taole Chen',
-  publisher: 'Taole Chen',
+  authors: [{ name: "Taole Chen", url: `${process.env.NEXT_PUBLIC_BASE_URL}` }],
+  creator: "Taole Chen",
+  publisher: "Taole Chen",
   openGraph: {
-    title: 'Taole Chen - Art Portfolio',
-    description: 'The art portfolio of Taole Chen, a contemporary artist exploring the world and architecture through travel, plein air painting and mostly ink.',
+    title: "Taole Chen - Art Portfolio",
+    description:
+      "The art portfolio of Taole Chen, a contemporary artist exploring the world and architecture through travel, plein air painting and mostly ink.",
     siteName: "Taole Chen's Art Portfolio",
     url: `${process.env.NEXT_PUBLIC_BASE_URL}`,
-    type: 'website',
+    type: "website",
     images: [
       {
         url: `${process.env.NEXT_PUBLIC_BASE_URL}/og-image.jpg`,
         width: 1200,
         height: 630,
-        alt: 'Taole Chen Art Portfolio',
+        alt: "Taole Chen Art Portfolio",
       },
     ],
   },
@@ -43,15 +63,16 @@ export const metadata: Metadata = {
     follow: true,
   },
   twitter: {
-    title: 'Taole Chen - Art Portfolio',
-    description: 'The art portfolio of Taole Chen, a contemporary artist exploring the world and architecture through travel, plein air painting and mostly ink.',
-    card: 'summary_large_image',
-    creator: '@payloadcms',
+    title: "Taole Chen - Art Portfolio",
+    description:
+      "The art portfolio of Taole Chen, a contemporary artist exploring the world and architecture through travel, plein air painting and mostly ink.",
+    card: "summary_large_image",
+    creator: "@payloadcms",
   },
-}
-
+};
 
 const RootLayout = async ({ children }: Props) => {
+  const logos = await fetchLogos();
 
   return (
     <html className={`${montserrat.className}`} lang="en">
@@ -61,7 +82,11 @@ const RootLayout = async ({ children }: Props) => {
       </head>
 
       <body>
+        <DotGrid />
+        <Header logo={logos && logos[0]?.logo} />
         {children}
+        <Footer />
+        <DashedGutterOverlay />
         <SpeedInsights />
       </body>
     </html>
