ce3b21d1c334d4bdcbd735293b4f73d36b951db3
Tao <taole.chen@gmail.com>
2025-09-20 11:51:49 +0200

    fixed root layout.

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  trailingSlash: false,
   images: {
     remotePatterns: [
       {
-                protocol: 'https',
-                hostname: "taos-pullzone.b-cdn.net",
-                port: '',
-                pathname: '/media/**',
-            },
-    ]
-  }
+        protocol: "https",
+        hostname: "taos-pullzone.b-cdn.net",
+        port: "",
+        pathname: "/media/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
